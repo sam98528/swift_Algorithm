65a7725968121f0dbc32a31ba719fb020e92827b
Seungwon Lee <sam98528@naver.com>
2024-01-19 02:07:30 +0900

    Update 가운데 글자 가져오기.swift

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -1,5 +1,8 @@
 func solution(_ s:String) -> String {
+    /* 내가 푼 방법
+    
     var ans : String = ""
+    
     if s.count % 2 == 0 {
         let lastIndex = s.index(s.startIndex, offsetBy:s.count / 2)
         let firstIndex = s.index(s.startIndex, offsetBy:s.count / 2 - 1)
@@ -8,5 +11,19 @@ func solution(_ s:String) -> String {
         let firstIndex = s.index(s.startIndex, offsetBy:s.count / 2)
         ans = String(s[firstIndex])
     }
+    
     return ans
+    */
+
+    // 더 좋은 방법
+    
+    let middleIndex = s.index(s.startIndex, offsetBy: s.count / 2)
+    
+    if s.count % 2 == 0 {
+        let previousIndex = s.index(before: middleIndex)
+        return String(s[previousIndex...middleIndex])
+    } else {
+        return String(s[middleIndex])
+    }
+    
 }
