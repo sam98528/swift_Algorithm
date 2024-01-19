d7bc5273dfafaaa9ba6990bc665df9b4d5b6bc36
Seungwon Lee <sam98528@naver.com>
2024-01-20 00:00:35 +0900

    Update 약수의 개수와 덧셈.swift

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -21,3 +21,12 @@ func solution(_ left:Int, _ right:Int) -> Int {
     }
     return ans
 }
+
+/*
+//다른 사람의 풀이 map 및 closure 사용.
+
+func solution(_ left: Int, _ right: Int) -> Int {
+    return (left...right).map { i in (1...i).filter { i % $0 == 0 }.count % 2 == 0 ? i : -i }.reduce(0, +)
+}
+
+*/
