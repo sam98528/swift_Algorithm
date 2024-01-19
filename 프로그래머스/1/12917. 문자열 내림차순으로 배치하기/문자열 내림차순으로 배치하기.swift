5e568480e5866fc0ad43ed8ad4774da22f08da0d
Seungwon Lee <sam98528@naver.com>
2024-01-20 00:19:19 +0900

    Update 문자열 내림차순으로 배치하기.swift

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
@@ -1,5 +1,5 @@
 /*
-
+// 처음에 내가 직접 풀어본 방법. String의 sorted 함수가 있는 줄 몰랐다.
 func solution(_ s:String) -> String {
     var temp = s
     var arr : [String] = []
@@ -15,6 +15,8 @@ func solution(_ s:String) -> String {
 */
 
 /*
+// 두번째로는 sorted 이용하고 reversed()까지 이용해서 푼 방법
+
 
 func solution(_ s:String) -> String {
     var a = String(String(s.sorted()).reversed())
@@ -22,6 +24,7 @@ func solution(_ s:String) -> String {
 }
 
 */
+//마지막으로는 reverse말고 애초에 sort할때 내림차순으로 설정하면된다. 
 func solution(_ s:String) -> String {
     return String(s.sorted{$0 > $1})
 }
