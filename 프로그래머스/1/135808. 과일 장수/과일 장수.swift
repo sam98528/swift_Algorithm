3f959a49a8ed7270b606552da87d89a12a4415db
Seungwon Lee <sam98528@naver.com>
2024-01-25 01:04:52 +0900

    [level 1] Title: 과일 장수, Time: 0.14 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,22 +1,12 @@
 import Foundation
-extension Array {
-    func chunked(into size: Int) -> [[Element]] {
-        return stride(from: 0, to: count, by: size).map {
-            Array(self[$0 ..< Swift.min($0 + size, count)])
-        }
-    }
-}
 
 func solution(_ k: Int, _ m: Int, _ score: [Int]) -> Int {
     var scores = Array(score.sorted().reversed())
+    var index = m-1
     var ans = 0
-    
-    let chunkedArray = scores.chunked(into: m)
-    for chunk in chunkedArray {
-        if chunk.count == m {
-            var min = chunk.min()!
-            ans += min * m
-        }
-    }
+    while index < score.count {
+        ans += scores[index] * m 
+        index += m
+    } 
     return ans
 }
