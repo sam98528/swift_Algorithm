c0ddf8c7692137041610a368739ee14f80cec139
Seungwon Lee <sam98528@naver.com>
2024-01-25 01:34:22 +0900

    Update 모의고사.swift

--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -42,3 +42,23 @@ func solution(_ answers:[Int]) -> [Int] {
     
     return ans
 }
+
+/* 더 나은 풀이 
+func solution(_ answers:[Int]) -> [Int] {
+    let answer = (
+        a: [1, 2, 3, 4, 5], // index % 5
+        b: [2, 1, 2, 3, 2, 4, 2, 5], // index % 8
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // index % 10
+    )
+    var point = [1:0, 2:0, 3:0]
+
+    for (i, v) in answers.enumerated() {
+        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
+    }
+
+    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+}
+
+/*
