74ffe6552144d6b7c34abffdb7f1d012468a5dce
Seungwon Lee <sam98528@naver.com>
2024-02-07 13:26:09 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 192.47 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
@@ -1,14 +1,14 @@
 import Foundation
-
+//확인
 func solution(_ elements:[Int]) -> Int {
-    var numberSet = Set<Int>()
+    var ans = Set<Int>()
     for index in 0..<elements.count {
-        var number = 0
+        var num = 0
         for offset in 0..<elements.count {
             let validIndex = (index + offset) % elements.count
-            number += elements[validIndex]
-            numberSet.insert(number)
+            num += elements[validIndex]
+            ans.insert(num)
         }
     }
-    return numberSet.count
+    return ans.count
 }
