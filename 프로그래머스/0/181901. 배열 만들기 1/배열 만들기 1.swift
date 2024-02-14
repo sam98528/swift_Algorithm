04c21bdded01d17ebe2af71317f836cb970bf18c
Seungwon Lee <sam98528@naver.com>
2024-02-15 02:19:57 +0900

    [level 0] Title: 배열 만들기 1, Time: 1.42 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int, _ k:Int) -> [Int] {
+    var ans : [Int] = []
+    for i in 1...n{
+        if i % k == 0 {
+            ans.append(i)
+        }
+    }
+    return ans
+}
