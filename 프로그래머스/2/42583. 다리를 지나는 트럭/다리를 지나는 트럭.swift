3df1ee71fc6aa44cae9b718ec8e9ee381a4d628d
Seungwon Lee <sam98528@naver.com>
2024-03-06 11:00:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -1,23 +1,22 @@
 import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    var array = Array(repeating: 0, count: bridge_length)
-    var ans = 0
+    var bridge = Array(repeating: 0 , count: bridge_length)
     var truck_weights = truck_weights
-    var bridgeWeights = 0
+    var ans = 0
+    var currentCar = 0
     
-    while !array.isEmpty {
+    while(!bridge.isEmpty){
         ans += 1
-        bridgeWeights -= array.removeFirst()
-        if let a = truck_weights.first {
-            if a + bridgeWeights <= weight {
-                array.append(a)
-                bridgeWeights += truck_weights.removeFirst()
-            } else {
-                array.append(0)
+        currentCar -= bridge.removeFirst()
+        if let temp = truck_weights.first{
+            if temp + currentCar <=  weight{
+                bridge.append(temp)
+                currentCar += truck_weights.removeFirst()
+            }else{
+                bridge.append(0)
             }
         }
     }
-    
     return ans
 }
