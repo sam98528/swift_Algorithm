5299d8c5d2944d6c3392ebbfc1b1e42008833035
Seungwon Lee <sam98528@naver.com>
2024-03-14 09:30:31 +0900

    Update upload.py

--- upload.py
+++ upload.py
@@ -45,7 +45,7 @@ def main():
       
     for file in files:
       if category not in solveds:
-        content += "| <center>{} |[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+        content += "| <center>{}</center> |[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
         solveds.append(category)
         
   with open("README.md", "w") as fd:
