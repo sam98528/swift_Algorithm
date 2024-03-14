9d51dca554dccdcd29db5a031eabd9e13f1195db
Seungwon Lee <sam98528@naver.com>
2024-03-14 20:39:15 +0900

    Update script.py

--- script.py
+++ script.py
@@ -11,14 +11,20 @@ HEADER="""
 """
 
 def extract_submission_date(readme_path):
-    with open(readme_path, "r", encoding="utf-8") as f:
-        for line in f:
-            if "제출 일자" in line:
-                submission_date_str = next(f).strip()
+    try:
+        with open(readme_path, "r", encoding="utf-8") as f:
+            readme_lines = f.readlines()
+            for index, line in enumerate(readme_lines):
+                if "제출 일자" in line:
+                    submission_date_index = index + 2
+                    submission_date_str = readme_lines[submission_date_index].strip()
 
-                # 날짜를 파싱하여 datetime 객체로 변환
-                submission_date = datetime.strptime(submission_date_str, "%Y년 %m월 %d일 %H:%M:%S")
-                return submission_date
+                    # 날짜를 파싱하여 datetime 객체로 변환
+                    submission_date = datetime.strptime(submission_date_str, "%Y년 %m월 %d일 %H:%M:%S")
+                    return submission_date
+            print("Submission Date를 찾을 수 없습니다.")
+    except FileNotFoundError:
+        print("README.md 파일을 찾을 수 없습니다.")
 
 
 def main():
@@ -61,7 +67,10 @@ def main():
             if file == 'README.md':
                 if category not in solveds:
                     submission_date = extract_submission_date(os.path.join(root, file))
-                    content += "| {} |[링크]({})|{}|\n".format(category, parse.quote(os.path.join(root, file)), submission_date.strftime("%Y-%m-%d"))
+                    if submission_date:
+                        content += "| {} |[링크]({})|{}|\n".format(category, parse.quote(os.path.join(root, file)), submission_date.strftime("%Y-%m-%d"))
+                    else:
+                        content += "| {} |[링크]({})|{}|\n".format(category, parse.quote(os.path.join(root, file)), "제출 일자를 찾을 수 없음")
                     solveds.append(category)
                     directory_count += 1
 
