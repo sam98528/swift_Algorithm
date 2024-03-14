38b555389e2651b43d6610f3a1869a6a799324b6
Seungwon Lee <sam98528@naver.com>
2024-03-14 23:37:11 +0900

    Update script.py

--- script.py
+++ script.py
@@ -65,7 +65,7 @@ def main():
                 if category not in solveds:
                     submission_date , link = extract_submission_date(os.path.join(root, file))
                     if submission_date:
-                        entry = "| <center> [{}]({} </center>|<center> {} </center>|<center> {} </center>|<center>[Link]({}) </center>|<center> {} </center>|\n".format("문제 링크",link, "LV " + directory, category, parse.quote(os.path.join(root)), submission_date.strftime("%Y-%m-%d"))
+                        entry = "| <center> [문제 링크]({}) </center>|<center> {} </center>|<center> {} </center>|<center>[Link]({}) </center>|<center> {} </center>|\n".format(link, "LV " + directory, category, parse.quote(os.path.join(root)), submission_date.strftime("%Y-%m-%d"))
                     else:
                         entry = "| {} | {} |[Link]({})|{}|\n".format(directory, category, parse.quote(os.path.join(root, file)), "제출 일자를 찾을 수 없음")
                     sorting_entries.append(entry)
