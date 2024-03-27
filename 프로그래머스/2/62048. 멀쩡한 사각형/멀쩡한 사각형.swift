f7efbc39baa1d4c183d69f5c132acb53d8b51c0b
Seungwon Lee <sam98528@naver.com>
2024-03-27 09:45:45 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ w:Int, _ h:Int) -> Int64{
+    return Int64(w * h - (w + h - (gcd(w, h))))
+}
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a : gcd(b, a % b)
+}
